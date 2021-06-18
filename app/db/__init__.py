29b2dafc45971fc5e4614eb23b42f9d5e2026700
bluesatyr <shawnevans.music@gmail.com>
2021-06-18 10:24:00 -0400

    add Jinja filters format_date, format_url and format_plural

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -4,9 +4,31 @@ from sqlalchemy import create_engine, engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
+# `appcontext`
+from flask import g 
+
 load_dotenv()
 
 # connect to the database using env variable
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db(app):
+    Base.metadata.create_all(engine)
+    # run close_db whenever a context is destroyed
+    app.teardown_appcontext(close_db)
+
+def get_db():
+    if 'db' not in g:
+        # store db connection in `app context`
+        g.db = Session()
+
+    return g.db
+
+def close_db(e=None):
+    # attempt to find and remove db from the g object
+    db= g.pop('db', None)
+    # if db exists, end db connection
+    if db is not None:
+        db.close()
